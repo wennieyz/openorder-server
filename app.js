837c57c1301de922215703953a0b4127edf12d72
Wennie Zhang <wenniezhang@Wennies-MacBook-Air.local>
2024-07-14 12:28:56 -1000

    Connect BE with FE

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var cors = require('cors')
+
 
 var indexRouter = require('./routes/index')
 // var usersRouter = require('./routes/users');
@@ -57,9 +59,25 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+
+
+app.get('/marketplace', (req, res) => {
+  res.json({
+    brandName: 'Baggu',
+    imgUrl: 'src/assets/productImages/duck-bag.png',
+    productTitle: 'Duck Bag',
+    price: 10,
+    isPriceMin: true,
+    minQuantity: '1',
+    processingTime: '1-2 days',
+    variants: ['Yellow', 'Blue', 'Green'],
+    iconTags: ['Globe', 'Recycle', 'Leaf'],
+  })
+})
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
